41ee239052552a405779a4c28f710692b51ab8ec
Johan Burke <jburke.create@gmail.com>
2024-11-09 13:23:15 -0500

    feat: move lazy to init.lua and add initial cmp config.

--- init.lua
+++ init.lua
@@ -1,4 +1,25 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",  -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("minimal.opts")
-require("minimal.lazy")
+
+-- TODO: figure out how to load complete optionally
+require("lazy").setup({
+    spec = {
+        { import = "minimal.plugins" },
+        { import = "complete.plugins" }
+    }
+})
+
 require("minimal.mappings")
 require("minimal.autocmds")
