20be83b6ac5d136bebfec852a497795dc925fb3d
Johan Burke <jburke.create@gmail.com>
2024-11-29 21:43:07 -0500

    feat: switch to blink.cmp

--- lua/complete/lsp/csharp.lua
+++ lua/complete/lsp/csharp.lua
@@ -1,10 +1,3 @@
-local lspconfig = require("lspconfig")
-
 return {
-    setup = function(capabilities)
-        lspconfig.omnisharp.setup {
-            capabilities = capabilities,
-            cmd = { "dotnet", "/home/jburke/.local/bin/Omnisharp/OmniSharp.dll" }
-        }
-    end
+    cmd = { "dotnet", "/home/jburke/.local/bin/Omnisharp/OmniSharp.dll" }
 }
--- lua/complete/plugins/lsp.lua
+++ lua/complete/plugins/lsp.lua
@@ -1,61 +1,20 @@
 return {
-    "hrsh7th/nvim-cmp",
-    event = { "InsertEnter", "CmdlineEnter" },
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "neovim/nvim-lspconfig",
-        "zbirenbaum/copilot-cmp"
+    'saghen/blink.cmp',
+    lazy = false,
+
+    version = 'v0.*',
+    opts = {
+        keymap = { preset = 'default',
+            ['<Tab>'] = { 'select_next', 'fallback' },
+            ['<S-Tab>'] = { 'select_prev', 'fallback' },
+            ['<CR>'] = { 'accept', 'fallback' }
+        },
+        nerd_font_variant = 'mono'
     },
-    config = function()
-        local cmp = require("cmp")
 
-        cmp.setup {
-            mapping = {
-                ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-                ['<Enter>'] = cmp.mapping.confirm({ select = true }),
-                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort()
-            },
-            sources = cmp.config.sources(
-                {
-                    { name = 'nvim_lsp' }
-                },
-                {
-                    { name= "path" }
-                },
-                {
-                    { name = "buffer" }
-                },
-                {
-                    { name = "copilot" }
-                }
-            )
+    sources = {
+        completion = {
+            enabled_providers = { 'lsp', 'path', 'buffer' }
         }
-
-        cmp.setup.cmdline({ '/', '?' }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = 'buffer' }
-            }
-        })
-
-        cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "path" }
-            },
-            {
-                { name = "cmdline" }
-            }),
-            matching = { disallow_symbol_nonprefix_matching = false }
-        })
-
-        require("complete.lsp").setup()
-    end
+    }
 }
